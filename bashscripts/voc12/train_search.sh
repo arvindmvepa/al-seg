e7f4e0106f9703b6cc0169fc9ae8643bca88e6b2
Arvind Vepa <arvind.m.vepa@gmail.com>
2022-05-17 05:33:51 -0700

    added scripts for mixed annotations experiment (1 annotation per image)

---
+++ bashscripts/voc12/train_search.sh
@@ -0,0 +1,10 @@
+# search through hyper-params
+file=train_spml_scribbles50_itags50.sh
+GPUS=0
+for params in "0.1 0.5" "0.2 0.75" "0.3 1.0" ; do
+  IMG_SIM_LOSS_WEIGHT=$(echo $params | sed -E 's/(.*) (.*)/\1/')
+  SEM_OCC_LOSS_WEIGHT=$(echo $params | sed -E 's/(.*) (.*)/\2/')
+  export IMG_SIM_LOSS_WEIGHT
+  export SEM_OCC_LOSS_WEIGHT
+  source $file
+done
