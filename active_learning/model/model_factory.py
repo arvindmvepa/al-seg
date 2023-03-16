5569e80b909e78b214d4da17ea7e024a94cb3bb6
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-03-16 12:51:23 -0700

    small fix

--- active_learning/model/model_factory.py
+++ active_learning/model/model_factory.py
@@ -1,4 +1,5 @@
 from active_learning.model.spml_model import SPMLModel
+from active_learning.model.wsl4mis_model import DMPLSModel
 
 
 class ModelFactory:
@@ -10,6 +11,8 @@ class ModelFactory:
     def create_model(model_type, **model_kwargs):
         if model_type == "spml":
             model = SPMLModel(**model_kwargs)
+        elif model_type == "dmpls":
+            model = DMPLSModel(**model_kwargs)
         else:
             raise ValueError(f"There is no model_type {model_type}")
         return model
