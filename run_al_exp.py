c0c2c61ae4c6212d2a4d488aeab853f459e8fb57
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-03-16 13:21:56 -0700

    add libraries to path

--- run_al_exp.py
+++ run_al_exp.py
@@ -1,5 +1,8 @@
 from active_learning.policy.policy_builder import PolicyBuilder
+import sys
 
 if __name__ == "__main__":
+    sys.path.append("./spml")
+    sys.path.append("./wsl4mis")
     policy = PolicyBuilder.build_policy("exp.yml")
     policy.run()
