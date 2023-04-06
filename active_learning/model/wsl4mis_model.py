f76a552923ea1620bc1b39217b2d496301f81652
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-04-05 17:18:03 -0700

    debug

--- active_learning/model/wsl4mis_model.py
+++ active_learning/model/wsl4mis_model.py
@@ -129,6 +129,7 @@ class DMPLSModel(BaseModel):
                     writer.add_image('train/Image', image, iter_num)
                     outputs = torch.argmax(torch.softmax(
                         outputs, dim=1), dim=1, keepdim=True)
+                    print(f"outputs.shape: {outputs.shape}, label_batch.shape: {label_batch.shape}")
                     writer.add_image('train/Prediction',
                                      outputs[1, ...] * 50, iter_num)
                     labs = label_batch[1, ...].unsqueeze(0) * 50
