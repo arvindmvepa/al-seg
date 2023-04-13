3a2cb37f89851402efdc9ec78d9a6cc21bc53596
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-04-12 19:30:27 -0700

    bug in factory code

--- active_learning/model/model_factory.py
+++ active_learning/model/model_factory.py
@@ -1,5 +1,3 @@
-from active_learning.model.spml_model import SPMLModel
-from active_learning.model.wsl4mis_model import DMPLSModel
 import sys
 
 
@@ -11,9 +9,11 @@ class ModelFactory:
     @staticmethod
     def create_model(model_type, **model_kwargs):
         if model_type == "spml":
+            from active_learning.model.spml_model import SPMLModel
             model = SPMLModel(**model_kwargs)
         elif model_type == "dmpls":
             sys.path.append("./wsl4mis/code")
+            from active_learning.model.wsl4mis_model import DMPLSModel
             model = DMPLSModel(**model_kwargs)
         else:
             raise ValueError(f"There is no model_type {model_type}")
