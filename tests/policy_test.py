50c4e13da78b71288354a3e81de47093a784bf2a
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-05-08 02:28:53 -0700

    progress

--- tests/policy_test.py
+++ tests/policy_test.py
@@ -8,17 +8,17 @@ import os
 
 
 @pytest.fixture
-def random_policy(mocker, file_keys, im_key, train_files_dict, rounds):
+def get_random_policy(mocker, file_keys, im_key, train_files_dict, rounds, ann_file):
     """
     Mock model and model_uncertainty and create random policy object
     """
     # mock BaseModel
     mock_base_model = mocker.Mock(spec=BaseModel)
-    mock_base_model.all_train_files_dict = mocker.Mock(return_value=train_files_dict)
-    mocker.patch.multiple(BaseModel, __abstractmethods__=set(), 
-                          get_round_train_file_paths=mocker.Mock(return_value="test.txt"),
-                          file_keys=mocker.Mock(return_value=file_keys),
-                          im_key=mocker.Mock(return_value=im_key))
+    mock_base_model.all_train_files_dict = train_files_dict
+    mock_base_model.file_keys = file_keys
+    mock_base_model.im_key = im_key
+    mocker.patch.multiple(BaseModel, __abstractmethods__=set(),
+                          get_round_train_file_paths=mocker.Mock(return_value=ann_file))
     # mock BaseModelUncertainty
     mock_base_model_uncertainty = mocker.Mock(spec=BaseModelUncertainty)
     mocker.patch.multiple(BaseModelUncertainty, __abstractmethods__=set())
@@ -28,16 +28,19 @@ def random_policy(mocker, file_keys, im_key, train_files_dict, rounds):
                                         rounds=rounds)
     yield policy
     # remove test.txt
-    os.remove("test.txt")
+    if os.path.exists(ann_file):
+        os.remove(ann_file)
 
 
-@pytest.mark.parametrize("file_keys, im_key, train_files_dict, rounds, exp_round1_count", 
-                         [(["images"], "images", {"images": [1, 2, 3, 4]}, [(0.25, 0.25)], 1)])
-def test_random_split(random_policy, file_keys, im_key, train_files_dict, rounds, exp_round1_count):
+@pytest.mark.parametrize("file_keys, im_key, train_files_dict, rounds, ann_file, exp_round1_count, ann_file", 
+                         [(["images"], "images", {"images": [1, 2, 3, 4]}, [(0.25, 0.25)], "test.txt", 1)])
+def test_random_split(get_random_policy, file_keys, im_key, train_files_dict, 
+                      rounds, ann_file, exp_round1_count):
     """
     Test that random_split works correctly
     """
-    random_split_dict = random_policy(file_keys, im_key, train_files_dict, rounds).random_split()
+    random_policy = get_random_policy(file_keys, im_key, train_files_dict, rounds, ann_file)
+    random_split_dict = random_policy.random_split()
     assert len(random_split_dict["images"]) == exp_round1_count
 
 
