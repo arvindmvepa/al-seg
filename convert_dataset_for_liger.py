40de377be27facdd8647e8ec2d3b6e4572b9c887
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-06-13 15:18:35 -0700

    add liger testing stuff

---
+++ convert_dataset_for_liger.py
@@ -0,0 +1,10 @@
+with open('spml/datasets/voc12/seambox_train+_a6_th0.5_hed.txt', 'r') as f:
+    lines = f.readlines()
+
+new_lines = []
+for line in lines:
+    new_line = line.replace('seambox_a6_th0.5', 'liger')
+    new_lines.append(new_line)
+
+with open('spml/datasets/voc12/train+liger_hed.txt', 'w') as f:
+    f.writelines(new_lines)
