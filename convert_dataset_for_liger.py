7f0d01fe59eae478d198220660b319f9833a7db3
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-06-13 15:32:07 -0700

    bug

--- convert_dataset_for_liger.py
+++ convert_dataset_for_liger.py
@@ -3,7 +3,7 @@ with open('spml/datasets/voc12/seambox_train+_a6_th0.5_hed.txt', 'r') as f:
 
 new_lines = []
 for line in lines:
-    new_line = line.replace('seambox_a6_th0.5', 'liger')
+    new_line = line.replace('seambox_a6_th0.5', 'liger_orig')
     new_lines.append(new_line)
 
 with open('spml/datasets/voc12/train+liger_hed.txt', 'w') as f:
