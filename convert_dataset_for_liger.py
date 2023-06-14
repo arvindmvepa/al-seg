e0220cabe7a428b6473f30e33b1615ba3d85b3f8
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-06-13 19:29:22 -0700

    add stuff for liger100

--- convert_dataset_for_liger.py
+++ convert_dataset_for_liger.py
@@ -1,10 +1,19 @@
+num_clusters = 100
+
+if num_clusters:
+    replace_string = f'liger_num_clusters{num_clusters}' 
+    new_file = f'spml/datasets/voc12/train+liger_num_clusters{num_clusters}_hed.txt'
+else:
+    replace_string = 'liger_orig'
+    new_file = 'spml/datasets/voc12/train+liger_hed.txt'
+
 with open('spml/datasets/voc12/seambox_train+_a6_th0.5_hed.txt', 'r') as f:
     lines = f.readlines()
 
 new_lines = []
 for line in lines:
-    new_line = line.replace('seambox_a6_th0.5', 'liger_orig')
+    new_line = line.replace('seambox_a6_th0.5', replace_string)
     new_lines.append(new_line)
 
-with open('spml/datasets/voc12/train+liger_hed.txt', 'w') as f:
+with open(new_file, 'w') as f:
     f.writelines(new_lines)
