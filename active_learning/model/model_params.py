1604cc8accb98ffb1bf3a3c416b22b2b82d531f1
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-06-14 01:21:01 -0700

    add liger top3 anns

--- active_learning/model/model_params.py
+++ active_learning/model/model_params.py
@@ -22,8 +22,22 @@ model_params = {
                       {
                           "train_split": "train+",
                           "left_base_im_list": "train+",
-                          "right_base_im_list": "liger10_hed.txt",
+                          "right_base_im_list": "liger_num_clusters10_hed.txt",
                           "orig_train_im_list_file": os.path.join("spml", "datasets", "voc12", "train+liger_num_clusters10_hed.txt"),
+                          "left_base_pim_list": "panoptic_train+",
+                          "right_base_pim_list": "hed.txt",
+                          "orig_train_pim_list_file" : os.path.join("spml", "datasets", "voc12",  "panoptic_train+_hed.txt"),
+                          "val_pim_list": os.path.join("spml", "datasets", "voc12", "panoptic_val.txt"),
+                          "exec_script": "spml/bashscripts/voc12/train_spml_box_al.sh",
+                          "train_results_dir": os.path.join("stage1", "results", "train+", "semantic_gray"),
+                          "train_logits_path": os.path.join("stage1", "results", "train+", "logits", "save_preds.npz")
+                      },
+                    "liger21_3":
+                      {
+                          "train_split": "train+",
+                          "left_base_im_list": "train+",
+                          "right_base_im_list": "liger_num_clusters21_top3_hed.txt",
+                          "orig_train_im_list_file": os.path.join("spml", "datasets", "voc12", "train+liger_num_clusters21_top3_anns_hed.txt"),
                           "left_base_pim_list": "panoptic_train+",
                           "right_base_pim_list": "hed.txt",
                           "orig_train_pim_list_file" : os.path.join("spml", "datasets", "voc12",  "panoptic_train+_hed.txt"),
@@ -36,7 +50,7 @@ model_params = {
                       {
                           "train_split": "train+",
                           "left_base_im_list": "train+",
-                          "right_base_im_list": "liger100_hed.txt",
+                          "right_base_im_list": "liger_num_clusters100_hed.txt",
                           "orig_train_im_list_file": os.path.join("spml", "datasets", "voc12", "train+liger_num_clusters100_hed.txt"),
                           "left_base_pim_list": "panoptic_train+",
                           "right_base_pim_list": "hed.txt",
