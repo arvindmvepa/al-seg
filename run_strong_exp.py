a86ae4efc79bc39f6459dba7442294e8bfc51caa
Zukang Yang <zukangy@Zukangs-MBP.attlocal.net>
2023-06-16 13:31:23 -0700

    removed comment

--- run_strong_exp.py
+++ run_strong_exp.py
@@ -3,6 +3,4 @@ from strong_supervision.models.model import RunExperiment
 
 if __name__ == '__main__':
     exp = RunExperiment("exp_mac_strong.yml")
-    # train_model(arch=model_architect, encoder_name=encoder_name, encoder_weights=encoder_weights,
-    #             data_root=data_root, snapshot_dir=snapshot_dir, gpus=gpus)
     exp.run()
