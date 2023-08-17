cc0bf9c4a8877dd50aa30301a79994d1f7c56314
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-08-17 02:25:26 -0700

    debugging

--- active_learning/model/db_scoring_functions.py
+++ active_learning/model/db_scoring_functions.py
@@ -2,18 +2,15 @@ import torch
 from active_learning.utils import seg_entropy_score
 
 
-def mean_probs(outputs):
+def pass_probs(outputs):
     print(f"outputs.shape: {outputs.shape}")
-    avg_outputs = torch.mean(outputs, dim=0)
-    print(f"avg_outputs.shape: {avg_outputs.shape}")
-    return avg_outputs
+    return outputs
 
 
 def entropy_score(outputs):
-    avg_outputs = torch.mean(outputs, dim=0)
-    entropy = seg_entropy_score(avg_outputs)
+    entropy = seg_entropy_score(outputs)
     return entropy
 
 
-db_scoring_functions = {"mean_probs": mean_probs,
+db_scoring_functions = {"pass_probs": pass_probs,
                         "entropy_score": entropy_score}
