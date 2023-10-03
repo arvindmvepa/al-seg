b6449df5f1705d87d0798dc7bb7aab2cb5ab10b1
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-03 04:09:15 -0700

    create experiment

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,9 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp_coreset_mdl_ft.yml", "exp_coregcn_mdl_ft.yml"]
+    exp_files = ["exp.yml", "exp1.yml", "exp2.yml", "exp3.yml", "exp4.yml", "exp5.yml", "exp6.yml", "exp7.yml",
+                 "exp8.yml", "exp9.yml", "exp10.yml", "exp11.yml", "exp12.yml", "exp13.yml", "exp14.yml", "exp15.yml",
+                 "exp16.yml", "exp17.yml", "exp18.yml", "exp19.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
