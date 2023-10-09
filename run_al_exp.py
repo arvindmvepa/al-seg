22379635e22d0137d812f4eed9132b07e8cc75b3
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-09 05:39:21 -0700

    update run file

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp_coreset.yml", "exp_db_dpmls.yml", "exp.yml"]
+    exp_files = ["exp_coreset_mdl_ft.yml", "exp_coregcn_mdl_ft.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
