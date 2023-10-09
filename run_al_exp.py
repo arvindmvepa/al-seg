4ab575ddb4cac78e63ee041c60a9275308f386a4
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-09 08:10:22 -0700

    new model features experiment

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,10 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp.yml", "exp1.yml", "exp2.yml", "exp3.yml", "exp4.yml", "exp5.yml", "exp6.yml", "exp7.yml",
-                 "exp8.yml", "exp9.yml", "exp10.yml", "exp11.yml", "exp12.yml", "exp13.yml", "exp14.yml", "exp15.yml",
-                 "exp16.yml", "exp17.yml", "exp18.yml", "exp19.yml", "exp20.yml", "exp21.yml", "exp22.yml", "exp23.yml",
-                 "exp24.yml"]
+    exp_files = ["exp45.yml", "exp46.yml", "exp47.yml", "exp48.yml", "exp49.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
