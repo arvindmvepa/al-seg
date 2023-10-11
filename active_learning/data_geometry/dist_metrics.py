84520d8575f0f7a8b6a0c5866176e874ca39c056
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-11 06:03:20 -0700

    save train cfgs

---
+++ active_learning/data_geometry/dist_metrics.py
@@ -0,0 +1,2 @@
+def custom_metric(image_vec1, image_vec2, config1, config2):
+    pass
---
+++ active_learning/misc.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+
+def save_train_cfg(data_dir, save_dir):
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    patient_dirs = os.listdir(data_dir)
+    for patient_dir in patient_dirs:
+        info_path = os.path.join(data_dir, patient_dir, "Info.cfg")
+        save_info_path = os.path.join(save_dir, patient_dir + ".cfg")
+        if os.path.exists(info_path):
+            shutil.copyfile(info_path, save_info_path)
+
+if __name__ == "__main__":
+    data_dir = r"C:\Users\Arvind\Downloads\Resources"
+    save_dir = r"C:\Users\Arvind\Documents\al-seg-arvind\wsl4mis_data\ACDC_training_cfgs"
+    save_train_cfg(data_dir, save_dir)
