e3acbeb67ed067c26d9515fa7e9b96c62b42f8ed
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-11 06:03:20 -0700

    load cfgs, update metric to include additional features, and update adjacency matrix generation

--- active_learning/data_geometry/coregcn_coreset.py
+++ active_learning/data_geometry/coregcn_coreset.py
@@ -4,13 +4,14 @@ import torch
 import torch.optim as optim
 from active_learning.data_geometry.base_coreset import BaseCoreset
 from active_learning.data_geometry.gcn import GCN
+from tqdm import tqdm
 
 
 class CoreGCN(BaseCoreset):
     """Class for identifying representative data points using Coreset sampling"""
 
     def __init__(self, subset_size="all", hidden_units=128, dropout_rate=0.3, lr_gcn=1e-3, wdecay=5e-4, lambda_loss=1.2,
-                 num_epochs_gcn=200, s_margin=0.1, starting_sample=5, **kwargs):
+                 num_epochs_gcn=200, s_margin=0.1, starting_sample=5, adj_sim_wt_metric=None, **kwargs):
         super().__init__(**kwargs)
         self.subset_size = subset_size
         self.hidden_units = hidden_units
@@ -21,6 +22,12 @@ class CoreGCN(BaseCoreset):
         self.num_epochs_gcn = num_epochs_gcn
         self.s_margin = s_margin
         self.starting_sample = starting_sample
+        if adj_sim_wt_metric is None:
+            self.adj_sim_wt_metric = None
+        elif adj_sim_wt_metric not in wt_metrics:
+            raise ValueError(f"adj_sim_wt_metric must be None or one of {list(wt_metrics.keys())}. Instead got {adj_sim_wt_metric}")
+        else:
+            self.adj_sim_wt_metric = wt_metrics[adj_sim_wt_metric]
         
     def calculate_representativeness(self, im_score_file, num_samples, prev_round_dir, train_logits_path,
                                      already_selected=[], skip=False, delete_preds=True, **kwargs):
@@ -58,7 +65,9 @@ class CoreGCN(BaseCoreset):
             else:
                 raise ValueError("feat must be a numpy array")
             features = nn.functional.normalize(features)
+            print("Getting adjacency matrix...")
             adj = self.aff_to_adj(features)
+            print("Finished.")
 
             gcn_module = GCN(nfeat=features.shape[1],
                              nhid=self.hidden_units,
@@ -94,7 +103,7 @@ class CoreGCN(BaseCoreset):
 
                 feat = feat.detach().cpu().numpy()
                 coreset_inst = self.create_coreset_inst(feat)
-                sample_indices += coreset_inst.select_batch_(lbl, num_samples)
+                sample_indices += coreset_inst.select_batch_(lbl.tolist(), num_samples)
 
                 print("Max confidence value: ", torch.max(scores.data).item())
                 print("Mean confidence value: ", torch.mean(scores.data).item())
@@ -119,9 +128,27 @@ class CoreGCN(BaseCoreset):
 
         return [self.all_train_im_files[i] for i in sample_indices]
 
+
     def aff_to_adj(self, x, y=None, eps=1e-10):
-        x = x.detach().cpu().numpy()
-        adj = np.matmul(x, x.transpose())
+        num_ims = len(self.all_train_im_files)
+        assert num_ims > 1, "Number of images must be greater than 1"
+        num_features = x.shape[1]
+        assert num_features > 1, "Number of features must be greater than 1"
+        if self.adj_sim_wt_metric is not None:
+            adj = np.eye(num_ims)
+            for i in tqdm(range(num_ims)):
+                slice_no = self.get_slice_no(i)
+                cur_index = i + 1
+                cur_slice_no = self.get_slice_no(i)
+                while cur_slice_no != 0 and cur_index < num_ims:
+                    wt = self.adj_sim_wt_metric(slice_no, cur_slice_no)
+                    adj[i, cur_index] = wt
+                    adj[cur_index, i] = wt
+                    cur_index += 1
+                    cur_slice_no = self.get_slice_no(i)
+        else:
+            x = x.detach().cpu().numpy()
+            adj = np.matmul(x, x.transpose())
         adj += -1.0 * np.eye(adj.shape[0])
         adj_diag = np.sum(adj, axis=0)  # rowise sum
         adj = np.matmul(adj, np.diag(1 / (adj_diag + eps)))
@@ -130,6 +157,12 @@ class CoreGCN(BaseCoreset):
 
         return adj
 
+    def get_slice_no(self, image_index):
+        if (0 <= image_index) and (image_index >= self.image_cfgs_arr.shape[0]):
+            return None
+        else:
+            return self.image_cfgs_arr[image_index, self.slice_pos_starting_index:self.slice_pos_ending_index]
+
     def BCEAdjLoss(self, scores, lbl, nlbl, l_adj, eps=1e-10):
         lnl = torch.log(scores[lbl] + eps)
         lnu = torch.log(1 - scores[nlbl] + eps)
@@ -137,3 +170,27 @@ class CoreGCN(BaseCoreset):
         unlabeled_score = torch.mean(lnu)
         bce_adj_loss = -labeled_score - l_adj*unlabeled_score
         return bce_adj_loss
+
+
+def get_k_slice_wt(orig_slice_pos, other_slice_pos, k=1):
+    if np.abs(orig_slice_pos - other_slice_pos) <= k:
+        return 1
+    else:
+        return 0
+
+def get_inv_slice_wt(orig_slice_pos, other_slice_pos):
+    if orig_slice_pos == other_slice_pos:
+        return 1
+    else:
+        return 1/(np.abs(orig_slice_pos - other_slice_pos))
+
+def get_exp_slice_wt(orig_slice_pos, other_slice_pos):
+    if orig_slice_pos == other_slice_pos:
+        return 1
+    else:
+        return 1/(np.exp(np.abs(orig_slice_pos - other_slice_pos) -1))
+
+
+wt_metrics = {"k_slice_wt": get_k_slice_wt,
+              "inv_slice_wt": get_inv_slice_wt,
+              "exp_slice_wt": get_exp_slice_wt}
