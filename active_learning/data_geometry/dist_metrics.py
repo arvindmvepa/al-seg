19e79e705e4730523c0e3a297fecd76e2b0a0d9c
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-12 02:31:24 -0700

    change default weight

--- active_learning/data_geometry/dist_metrics.py
+++ active_learning/data_geometry/dist_metrics.py
@@ -5,7 +5,7 @@ from numpy.linalg import norm
 def euclidean_w_config(image_vec1, image_vec2, num_im_features, phase_starting_index, phase_ending_index,
                        group_starting_index, group_ending_index, height_starting_index, height_ending_index,
                        width_starting_index, width_ending_index, slice_pos_starting_index, slice_pos_ending_index,
-                       extra_feature_weight=5):
+                       extra_feature_weight=1.0):
     l2_norm_val = norm(image_vec1[:num_im_features], image_vec2[:num_im_features])
     phase_score = 1 - np.abs(image_vec1[phase_starting_index:phase_ending_index] - image_vec2[phase_starting_index:phase_ending_index])
     group_score = 1 - np.abs(image_vec1[group_starting_index:group_ending_index] - image_vec2[group_starting_index:group_ending_index])
