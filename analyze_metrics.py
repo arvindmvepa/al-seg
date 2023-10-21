aa4ad535be0eb822c0cfd611f657eaff72fe18d8
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-20 20:25:17 -0700

    updated script

--- analyze_metrics.py
+++ analyze_metrics.py
@@ -15,14 +15,16 @@ for exp_dir in exp_dirs:
         round_results = []
         round_dirs = sorted(list(glob(os.path.join(exp_dir, "round*"))))
         for round_dir in round_dirs:
-            model_dirs = sorted(list(glob(os.path.join(round_dir, "*"))))
-            val_max = 0.0
-            test_for_val_max = 0.0
+            model_dirs = sorted([dirpath for dirpath in list(glob(os.path.join(round_dir, "*"))) if os.path.isdir(dirpath)])
+            val_max = None
+            test_for_val_max = None
+            num_models = 0
             for model_dir in model_dirs:
+                print(model_dir)
                 val_metric_dict = None
                 test_metric_dict = None
-                val_metric_file = os.path.join(exp_dir, round_dir, model_dir, "val_metrics.json")
-                test_metric_file = os.path.join(exp_dir, round_dir, model_dir, "test_metrics.json")
+                val_metric_file = os.path.join(model_dir, "val_metrics.json")
+                test_metric_file = os.path.join(model_dir, "test_metrics.json")
                 if os.path.exists(val_metric_file):
                     with open(val_metric_file) as val_metric_fp:
                         val_metric_dict = json.load(val_metric_fp)
@@ -31,19 +33,29 @@ for exp_dir in exp_dirs:
                         test_metric_dict = json.load(test_metric_fp)
                 if (val_metric_dict is None) or (test_metric_dict is None):
                     continue
+                print(val_metric_dict)
+                print(test_metric_dict)
+
                 val_result = val_metric_dict["performance"]
-                if val_result > val_max:
+                test_result = test_metric_dict["performance"]
+
+                if val_max is None:
                     val_max = val_result
-                    test_for_val_max = test_metric_dict["performance"]
-            round_results.extend([val_max, test_for_val_max])
+                    test_for_val_max = test_result
+                elif val_result > val_max:
+                    val_max = val_result
+                    test_for_val_max = test_result
+                num_models += 1
+            if (val_max is not None) and (test_for_val_max is not None):
+                round_results.extend([val_max, test_for_val_max, num_models])
         if round_results:
             exp_results = [os.path.basename(exp_dir)]
             exp_results.extend(round_results)
             results.append(exp_results)
 
-headers = ["exp", "round0_val", "round0_test", "round1_val", "round1_test",
-           "round2_val", "round2_test","round3_val", "round3_test",
-           "round4_val", "round4_test", "round5_val", "round5_test"]
+headers = ["exp", "round0_val", "round0_test", "num_models", "round1_val", "round1_test", "num_models",
+           "round2_val", "round2_test", "num_models", "round3_val", "round3_test", "num_models",
+           "round4_val", "round4_test", "num_models", "round5_val", "round5_test", "num_models"]
 
 with open(results_csv_file, mode='w', newline='') as results_fp:
     writer = csv.writer(results_fp)
