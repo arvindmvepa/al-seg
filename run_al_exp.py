d43e5ee3d5a1d261909966c07e5f1046231e11fe
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-22 15:26:10 -0700

    new experiment

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp36.yml", "exp37.yml", "exp38.yml"]
+    exp_files = ["exp39.yml", "exp40.yml", "exp41.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
