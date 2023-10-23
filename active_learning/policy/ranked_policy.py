e0000fcd33a505aaa4c68799e54995ca52a99275
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-23 02:05:08 -0700

    bug

--- active_learning/policy/ranked_policy.py
+++ active_learning/policy/ranked_policy.py
@@ -27,13 +27,12 @@ class RankedPolicy(BaseActiveLearningPolicy):
     """
 
     def __init__(self, model, model_uncertainty=None, data_geometry=None, ensemble_kwargs=None, uncertainty_kwargs=None,
-                 save_im_score_file="scores.txt", rank_type="desc",
-                 rounds=(), exp_dir="test", pseudolabels=False,
+                 save_im_score_file="scores.txt", rank_type="desc", resume=False, rounds=(), exp_dir="test",
+                 pseudolabels=False,
                  tag="", seed=0):
         super().__init__(model=model, model_uncertainty=model_uncertainty, ensemble_kwargs=ensemble_kwargs,
                          uncertainty_kwargs=uncertainty_kwargs, data_geometry=data_geometry,
-                         rounds=rounds, pseudolabels=pseudolabels,
-                         exp_dir=exp_dir, tag=tag, seed=seed)
+                         rounds=rounds, resume=resume, pseudolabels=pseudolabels, exp_dir=exp_dir, tag=tag, seed=seed)
         self.save_im_score_file = save_im_score_file
         self.cur_im_score_file = None
         self.rank_type = rank_type
