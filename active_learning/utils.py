55d8b72c320ed9ec952eca4194846e0ad722026d
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-10-30 02:54:43 -0700

    bug

--- active_learning/utils.py
+++ active_learning/utils.py
@@ -9,8 +9,7 @@ def seg_entropy_score(class_seg_prob_tensor):
 
 def pixel_entropy_score(class_seg_prob_tensor):
     entropy_arr = class_seg_prob_tensor * torch.log(class_seg_prob_tensor + 1e-10)
-    # convert all nan values to 0
-    entropy_arr = torch.where(torch.isnan(entropy_arr), 0, entropy_arr)
+    entropy_arr = torch.nan_to_num(entropy_arr, nan=0.0)
     entropy_arr = -torch.sum(entropy_arr, dim=0)
 
     return entropy_arr
