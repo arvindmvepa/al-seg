39a6a8d2584daae2bc0ede9ec3973c01dd4f68f6
Arvind Vepa <arvind.m.vepa@gmail.com>
2023-11-01 23:54:17 -0700

    cl experiment with slice loss

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp39.yml", "exp40.yml", "exp41.yml"]
+    exp_files = ["exp144.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
