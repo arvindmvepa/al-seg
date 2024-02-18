e05dedbfcd91ec6a6ed39b2eee9ee534c6020a29
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-18 07:30:25 -0800

    exp changes thus far

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp_davis.yml"]
+    exp_files = ["exp399.yml", "exp400.yml", "exp401.yml", "exp402.yml", "exp403.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
