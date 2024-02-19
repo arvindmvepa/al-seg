0e6b458f73e0959bf3c4aadc08f564abf8958695
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-19 09:07:03 -0800

    MSCMR experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp399.yml", "exp400.yml", "exp401.yml", "exp402.yml", "exp403.yml"]
+    exp_files = ["exp448.yml", "exp449.yml", "exp450.yml", "exp451.yml", "exp452.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
