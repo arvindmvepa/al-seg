84b6a584529012a26f934f3e00e076467cd00cbd
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-20 00:42:59 -0800

    minor bug fixes

--- active_learning/policy/ranked_policy.py
+++ active_learning/policy/ranked_policy.py
@@ -57,7 +57,8 @@ class RankedPolicy(BaseActiveLearningPolicy):
     def _ranked_sample_unann_indices(self):
         unann_im_dict, num_samples = self._get_unann_train_file_paths(), self._get_unann_num_to_sample()
         # get the scores per image from the score file in this format: img_name, score
-        im_scores_list = open(self.cur_im_score_file).readlines()
+        with open(self.cur_im_score_file, "r") as f:
+            im_scores_list = f.readlines()
         im_scores_list = [im_score.strip().split(",") for im_score in im_scores_list]
         im_scores_list = [(im_score[0], float(im_score[1])) for im_score in im_scores_list]
         if self.rank_type == "desc":
