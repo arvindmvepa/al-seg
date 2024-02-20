cb9dc92f0d8dded55754ebda507a1c5f284482c9
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-20 01:19:40 -0800

    bug

--- active_learning/dataset/__init__.py
+++ active_learning/dataset/__init__.py
@@ -1,4 +1,5 @@
 from active_learning.dataset.acdc_dataset import ACDC_Dataset
 from active_learning.dataset.chaos_dataset import CHAOS_CT_Dataset
 from active_learning.dataset.lvsc_dataset import LVSC_Dataset
+from active_learning.dataset.mscmr_dataset import MSCMR_Dataset
 
