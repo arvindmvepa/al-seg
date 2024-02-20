77a43454f070433896c94a5fe10646b8c55e9bea
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-20 02:51:02 -0800

    mscmr coreset/contrastive/vaal experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,9 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp448.yml", "exp449.yml", "exp450.yml", "exp451.yml", "exp452.yml"]
+    exp_files = ["exp468.yml", "exp469.yml", "exp470.yml", "exp471.yml", "exp472.yml",
+                 "exp488.yml", "exp489.yml", "exp490.yml", "exp491.yml", "exp492.yml",
+                 ]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
