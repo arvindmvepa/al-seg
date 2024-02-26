8c8eace910778662a2f75b9cc016e5b30fae1b19
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-26 08:21:07 -0800

    bug

--- generate_test_predictions.py
+++ generate_test_predictions.py
@@ -4,7 +4,7 @@ from glob import glob
 
 
 root_dir = "."
-exp_dirs = sorted(list(glob(os.path.join(root_dir, "*", "DMPLS_*_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10*"))))
+exp_dirs = sorted(list(glob(os.path.join(root_dir, "DMPLS_*_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10*"))))
 exp_dirs = exp_dirs + sorted(list(glob(os.path.join(root_dir, "*", "DMPLS_*_random*_v10"))))
 
 results = []
