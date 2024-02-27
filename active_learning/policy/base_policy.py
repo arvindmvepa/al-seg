da44d3fdf94c4664e12641acd78387c59f425602
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-02-27 00:27:16 -0800

    add generate results code

--- active_learning/policy/base_policy.py
+++ active_learning/policy/base_policy.py
@@ -106,6 +106,12 @@ class BaseActiveLearningPolicy:
             self._setup_round()
             self._evaluate_round()
 
+    def generate(self, model_no, round_num, prediction_ims):
+        for i in range(self.num_rounds):
+            self._setup_round()
+            if i == round_num:
+                self._generate_round(model_no=model_no, prediction_ims=prediction_ims)
+
     def data_split(self):
         return self.random_split()
 
@@ -133,6 +139,9 @@ class BaseActiveLearningPolicy:
 
     def _evaluate_round(self):
         self._evaluate_round_models()
+
+    def _generate_round(self, model_no, prediction_ims):
+        self._generate_round_models(model_no=model_no, prediction_ims=prediction_ims)
 
     def _run_round_models(self, im_score_file=None, calculate_model_uncertainty=False, calculate_data_geometry=False,
                           ensemble_kwargs=None, uncertainty_kwargs=None, geometry_kwargs=None):
@@ -214,6 +223,10 @@ class BaseActiveLearningPolicy:
                                   cur_total_oracle_split=self.cur_total_oracle_split,
                                   cur_total_pseudo_split=self.cur_total_pseudo_split, train=False, inf_train=False,
                                   inf_val=False, inf_test=True)
+        torch.cuda.empty_cache()
+
+    def _generate_round_models(self, model_no, prediction_ims):
+        self.model.generate_results(round_dir=self.round_dir, model_no = model_no, prediction_ims=prediction_ims)
         torch.cuda.empty_cache()
 
     def _data_split(self, splt_func):
