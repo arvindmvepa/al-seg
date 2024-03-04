6086a8649f550bfdd6f03a8a8f838c3a165bbc47
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-03-03 21:05:53 -0800

    updated predictions for visualization

--- collect_test_results.py
+++ collect_test_results.py
@@ -4,9 +4,23 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 if __name__ == '__main__':
     root_dir = "."
-    #model_dirs = [r"/home/asjchoi/al-seg4/DMPLS_exp4_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10/round_4/1", r"C:\Users\Arvind\Documents\al-seg-arvind2\DMPLS_exp1_random_v10\round_4\2"]
-    model_dirs = [r"/home/asjchoi/al-seg4/DMPLS_exp4_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10/round_1/0", r"C:\Users\Arvind\Documents\al-seg-arvind2\DMPLS_exp1_random_v10\round_1\0"]
-    prediction_ims = ["patient146_frame01.h5", "patient113_frame01.h5"]
+    model_dirs = [r"/home/asjchoi/al-seg/DMPLS_exp0_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10/round_1/0",
+                  r"/home/asjchoi/al-seg/DMPLS_exp0_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10/round_2/0",
+                  r"/home/asjchoi/al-seg/DMPLS_exp0_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10/round_3/0",
+                  r"/home/asjchoi/al-seg/DMPLS_exp0_coreset_fuse20_pos_loss1_wt02_pos_loss2_wt01_pos_loss3_wt005_use_slice_pos_use_phase_use_patient_uncertainty1_v10/round_4/0",
+                  r"/home/amvepa91/al-seg2/DMPLS_exp4_coreset_v10/round_1/0",
+                  r"/home/amvepa91/al-seg2/DMPLS_exp4_coreset_v10/round_2/0",
+                  r"/home/amvepa91/al-seg2/DMPLS_exp4_coreset_v10/round_3/0",
+                  r"/home/amvepa91/al-seg2/DMPLS_exp4_coreset_v10/round_4/0",
+                  r"/home/amvepa91/al-seg1/DMPLS_exp3_coregcn_model_fts_ss10_rstart_v10/round_1/0",
+                  r"/home/amvepa91/al-seg1/DMPLS_exp3_coregcn_model_fts_ss10_rstart_v10/round_2/0",
+                  r"/home/amvepa91/al-seg1/DMPLS_exp3_coregcn_model_fts_ss10_rstart_v10/round_3/0",
+                  r"/home/amvepa91/al-seg1/DMPLS_exp3_coregcn_model_fts_ss10_rstart_v10/round_4/0",
+                  r"C:\Users\Arvind\Documents\al-seg-arvind2\DMPLS_exp1_random_v10\round_1\0",
+                  r"C:\Users\Arvind\Documents\al-seg-arvind2\DMPLS_exp1_random_v10\round_2\0",
+                  r"C:\Users\Arvind\Documents\al-seg-arvind2\DMPLS_exp1_random_v10\round_3\0",
+                  r"C:\Users\Arvind\Documents\al-seg-arvind2\DMPLS_exp1_random_v10\round_4\0"]
+    prediction_ims = ["patient117_frame13.h5", "patient150_frame12.h5", "patient133_frame10.h5"]
 
     for model_dir in model_dirs:
         model_pth = os.path.join(model_dir, "unet_cct_best_model.pth")
