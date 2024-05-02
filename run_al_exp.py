727f3a116600fd3bd8e3dcddd1c0e0366fdaa931
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-02 01:51:13 -0700

    add exp files for testing label wt

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp657.yml"]
+    exp_files = ["exp657.yml", "exp658.yml", "exp659.yml", "exp660.yml", "exp661.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
