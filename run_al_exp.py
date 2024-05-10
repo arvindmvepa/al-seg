014fc73a13f71f230350197a333c2a35222d4555
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-10 03:58:06 -0700

    new ablation experiments for single losses

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp677.yml", "exp678.yml", "exp679.yml", "exp680.yml", "exp681.yml"]
+    exp_files = ["exp697.yml", "exp698.yml", "exp699.yml", "exp700.yml", "exp701.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
