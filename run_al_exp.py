98fcc8fd3d507af58afa5d666ef78c062c4f2a96
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-11 19:26:13 -0700

    adjusted loss weights for two loss contrastive network and three loss contrastive network

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp697.yml", "exp698.yml", "exp699.yml", "exp700.yml", "exp701.yml"]
+    exp_files = ["exp737.yml", "exp738.yml", "exp739.yml", "exp740.yml", "exp741.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
