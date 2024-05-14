71c78129cb1f9f146e570963aee12307d8823091
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-14 05:52:57 -0700

    add new two loss ablation exp

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp787.yml", "exp788.yml", "exp789.yml", "exp790.yml", "exp791.yml"]
+    exp_files = ["exp797.yml", "exp798.yml", "exp799.yml", "exp800.yml", "exp801.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
