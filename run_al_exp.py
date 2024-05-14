3e91f1ba293be0257f9c824eb55775b230c1385c
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-13 18:32:18 -0700

    new ablation experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp772.yml", "exp773.yml", "exp774.yml", "exp775.yml", "exp776.yml"]
+    exp_files = ["exp787.yml", "exp788.yml", "exp789.yml", "exp790.yml", "exp791.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
