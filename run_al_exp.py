5599f08e7788e1af4276e10d4cb248b256b84726
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-17 14:10:15 -0700

    new set of experiments with just scribble experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp847.yml", "exp848.yml", "exp849.yml", "exp850.yml", "exp851.yml"]
+    exp_files = ["exp867.yml", "exp868.yml", "exp869.yml", "exp870.yml", "exp871.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
