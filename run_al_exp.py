179a81f1c37cd0324b01bf1f7de4399aeab4b3e0
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-18 05:17:26 -0700

    bug fix

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp867.yml", "exp868.yml", "exp869.yml", "exp870.yml", "exp871.yml"]
+    exp_files = ["exp887.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
