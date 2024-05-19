146e272ddf3ab595fd355241907eca469e93ce8a
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-19 03:41:50 -0700

    chaos experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     exp_files = ["exp922.yml", "exp923.yml", "exp924.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
-        policy.run()a
+        policy.run()
