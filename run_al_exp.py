0800a03f0af3731e45e17ff858f3c142816151ef
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-19 07:07:25 -0700

    new uncertainty experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp940.yml"]
+    exp_files = ["exp940.yml", "exp941.yml", "exp942.yml", "exp943.yml", "exp944.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
