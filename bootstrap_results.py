7bd0a983b655b49d78090762c216058a26af0c02
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-20 08:29:52 -0700

    debug

--- bootstrap_results.py
+++ bootstrap_results.py
@@ -192,22 +192,25 @@ for exp_dir in exp_dirs:
                     model_dir_for_val_max = model_dir
                 num_models += 1
             cur_results_file = os.path.join(round_dir, results_file)
-            if (not os.path.exists(cur_results_file)) or overwrite:
-                best_model_path = list(glob(os.path.join(model_dir_for_val_max, '*_best_model.pth')))
-                if len(best_model_path) > 0:
-                    best_model_path = best_model_path[0]
-                    seg_model = os.path.basename(best_model_path).split("_best_model.pth")[0]
-                    model = load_best_model(best_model_path, seg_model=seg_model)
-                    test_results = generate_test_predictions(model_for_val_max)
-                    bs_test_results = generate_bootstrap_results(test_results)
-                    save_results_to_file(bs_test_results, cur_results_file)
-                    confidence_interval = np.percentile(bs_test_results, [2.5, 97.5])
-                    print("result")
-                    print("95% Confidence Interval:", confidence_interval)
+            if model_dir_for_val_max is not None:
+                if (not os.path.exists(cur_results_file)) or overwrite:
+                    best_model_path = list(glob(os.path.join(model_dir_for_val_max, '*_best_model.pth')))
+                    if len(best_model_path) > 0:
+                        best_model_path = best_model_path[0]
+                        seg_model = os.path.basename(best_model_path).split("_best_model.pth")[0]
+                        model = load_best_model(best_model_path, seg_model=seg_model)
+                        test_results = generate_test_predictions(model_for_val_max)
+                        bs_test_results = generate_bootstrap_results(test_results)
+                        save_results_to_file(bs_test_results, cur_results_file)
+                        confidence_interval = np.percentile(bs_test_results, [2.5, 97.5])
+                        print("result")
+                        print("95% Confidence Interval:", confidence_interval)
+                    else:
+                        print("no model found")
                 else:
-                    print("model doesn't exist")
+                    print("results already exist")
             else:
-                print("results already exist")
+                print("no model found")
 # collect all the results
 results = get_ci_results(exp_dirs)
 print(results)
