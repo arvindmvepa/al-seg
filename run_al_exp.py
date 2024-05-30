afebb2ae3d05af80530efe59ef80ba276f47f4e2
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-05-30 04:58:50 -0700

    chaos experiments, minus ours and coregcn

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,6 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp922.yml", "exp923.yml", "exp924.yml"]
-    for exp_file in exp_files:
+    exp_files = ["exp934.yml", "exp935.yml",  "exp936.yml", "exp937.yml", "exp938.yml", "exp939.yml", "exp940.yml", "exp941.yml", "exp942.yml", "exp943.yml", "exp944.yml", "exp945.yml", "exp946.yml", "exp947.yml", "exp948.yml"]    for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
