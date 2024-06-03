56e2201fff1b935400e4c80ec3b5a51bb8aa0de1
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-06-02 21:41:06 -0700

    coregcn experiment

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp934.yml", "exp935.yml",  "exp936.yml", "exp937.yml", "exp938.yml", "exp939.yml", "exp940.yml", "exp941.yml", "exp942.yml", "exp943.yml", "exp944.yml", "exp945.yml", "exp946.yml", "exp947.yml", "exp948.yml"]
+    exp_files = ["exp959.yml", "exp960.yml", "exp961.yml", "exp962.yml", "exp963.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
