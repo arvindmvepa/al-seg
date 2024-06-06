db50532b695cf8170ebcf9f3ce34609fdd2d046f
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-06-05 22:21:51 -0700

    DAVIS experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,10 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp964.yml", "exp965.yml", "exp966.yml", "exp967.yml", "exp968.yml"]
+    exp_files = ["exp974.yml", "exp975.yml", "exp976.yml", "exp977.yml", "exp978.yml",
+                 "exp979.yml", "exp980.yml", "exp981.yml", "exp982.yml", "exp983.yml",
+                 "exp984.yml", "exp985.yml", "exp986.yml", "exp987.yml", "exp988.yml",
+                 "exp989.yml", "exp990.yml", "exp991.yml", "exp992.yml", "exp993.yml"]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
