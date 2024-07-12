75aca7ff928794a9a95dedb59ee740ee7f36eb3e
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-07-11 23:29:18 -0700

    new experiments

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,8 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    exp_files = ["exp940.yml", "exp941.yml", "exp942.yml", "exp943.yml", "exp944.yml"]
+    indices = list(range(950, 996))
+    exp_files = [f"exp{index}.yml" for index in indices]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
         policy.run()
