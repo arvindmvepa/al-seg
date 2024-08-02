93f44739133b03a27a2da0943ec5037fdc4aee41
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-08-02 06:08:29 -0700

    new experiment

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,11 +2,8 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    #indices = list(range(1055, 1065))
-    #exp_files = [f"exp{index}.yml" for index in indices]
-    #for exp_file in exp_files:
-    #    policy = PolicyBuilder.build_policy(exp_file)
-    #    policy.run()
-    exp_file = "exp_resenet50_coreset.yml"
-    policy = PolicyBuilder.build_policy(exp_file)
-    policy.run()
+    indices = list(range(1080, 1085))
+    exp_files = [f"exp{index}.yml" for index in indices]
+    for exp_file in exp_files:
+        policy = PolicyBuilder.build_policy(exp_file)
+        policy.run()
