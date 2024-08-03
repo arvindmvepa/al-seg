5c742f803ac8a911893368fb30391e7686e2fe1b
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-08-03 03:58:43 -0700

    new experiment with chaos

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    indices = list(range(1100, 1105))
+    indices = list(range(1120, 1125))
     exp_files = [f"exp{index}.yml" for index in indices]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
