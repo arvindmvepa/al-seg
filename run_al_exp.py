0622677818add07115030401be30074dbde19a0c
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-08-11 05:22:41 -0700

    bug

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    indices = list(range(1170, 1175))
+    indices = list(range(1185, 1186))
     exp_files = [f"exp{index}.yml" for index in indices]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
