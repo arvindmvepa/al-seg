ed60d41086bdfd7b30d4fb52dc107c0464272782
Arvind Vepa <arvind.m.vepa@gmail.com>
2024-08-11 05:58:31 -0700

    full pretrained experiment

--- run_al_exp.py
+++ run_al_exp.py
@@ -2,7 +2,7 @@ from active_learning.policy.policy_builder import PolicyBuilder
 
 
 if __name__ == "__main__":
-    indices = list(range(1185, 1186))
+    indices = list(range(1185, 1190))
     exp_files = [f"exp{index}.yml" for index in indices]
     for exp_file in exp_files:
         policy = PolicyBuilder.build_policy(exp_file)
